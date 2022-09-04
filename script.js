294fff61f30fcb7b70ad2c52c7972161b6e625a2
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-09-04 14:29:29 +0600

    2 counter in js

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const counterElement = document.getElementById('counter');
 const incrementElement = document.getElementById('increment');
 const decrementElement = document.getElementById('decrement');
 
+
+const counter2Element = document.getElementById('counter2');
+const increment2Element = document.getElementById('increment2');
+const decrement2Element = document.getElementById('decrement2');
+
 // Initial State
 let counter = 0;
+let counter2 = 0;
 
 // Event Listener
 incrementElement.addEventListener('click',()=>{
@@ -16,5 +22,18 @@ incrementElement.addEventListener('click',()=>{
 decrementElement.addEventListener('click',()=>{
     counter--;
     counterElement.innerText = counter;
+    
+})
+
+
+increment2Element.addEventListener('click',()=>{
+    counter2++;
+    counter2Element.innerText = counter2;
+    
+})
+
+decrement2Element.addEventListener('click',()=>{
+    counter2--;
+    counter2Element.innerText = counter2;
     
 })
